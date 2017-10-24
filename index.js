d304521d5fc0b48daaa5514fe67c28772aa88991
Shannon McCormack <shannonkmc@hotmail.com>
2017-10-24 20:29:13 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks (numberofBlocks) {
+  if (numberofBlocks > 42) {
+    return numberofBlocks - 42;
+  } else {
+    return 42 - numberofBlocks;
+  }
+}
