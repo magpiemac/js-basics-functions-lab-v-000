522b3e3c178d6c60d810f1ee201510df37f10e5c
Shannon McCormack <shannonkmc@hotmail.com>
2017-10-25 16:51:21 +0000

    Done.

--- index.js
+++ index.js
@@ -6,3 +6,28 @@ function distanceFromHqInBlocks (numberofBlocks) {
     return 42 - numberofBlocks;
   }
 }
+
+function distanceFromHqInFeet (numberofBlocks) {
+  return distanceFromHqInBlocks(numberofBlocks) * 264;
+}
+
+function distanceTravelledInFeet (startpoint, endpoint) {
+  if (startpoint < endpoint) {
+    return (endpoint - startpoint) * 264;
+ }  else {
+    return (startpoint - endpoint) * 264;
+  }
+}
+
+function calculatesFarePrice (startpoint, endpoint) {
+  const distance = distanceTravelledInFeet(startpoint, endpoint);
+  if (distance <= 400) {
+    return 0;
+  } else if (distance > 400 && distance < 2000) {
+    return distance * .02;
+  } else if (distance > 2000 && distance < 2500) {
+    return 25;
+  } else {
+    return 'cannot travel that far';
+  }
+}
