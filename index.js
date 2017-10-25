50f3a720902fcf026be0bd44a9c0c81df3c6f913
Shannon McCormack <shannonkmc@hotmail.com>
2017-10-25 16:43:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ function calculatesFarePrice (startpoint, endpoint) {
   const distance = distanceTravelledInFeet(startpoint, endpoint);
   if (distance <= 400) {
     return 0;
-  } else if (distance > 401 && distance < 2000) {
+  } else if (distance > 400 && distance < 2000) {
     return (distance * .02)
   }
 }
